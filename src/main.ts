7fbe1b51ea45cee12ebab8517476c602c0dd29f3
Enzo Braga <enzovasoncelosbraga@gmail.com>
2026-02-16 11:16:19 -0300

    chore(infra): configure prisma, global validation pipe and exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaExceptionFilter } from './commom/filters/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new PrismaExceptionFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({
